93c100d4afe5e2a2ee9b04c4efe98a464229413f
Jan Wirth <jan@scalab.app>
2019-10-07 17:01:30 +0200

    Improve

--- main.py
+++ main.py
@@ -4,16 +4,21 @@ import pynput.mouse
 import pynput.keyboard
 import time
 
+ATTACK_HOTKEY = 'q'
+MOVE_HOTKEY = 'w'
+
 mouse = pynput.mouse.Controller()
 keyboard = pynput.keyboard.Controller()
 
 def on_press(key):
     try:
-        if key.char == 'a':
+        if key.char == ATTACK_HOTKEY:
+            keyboard.press('a')
+            keyboard.release('a')
             time.sleep(0.15)
             mouse.press(Button.left)
             mouse.release(Button.left)
-        if key.char == 'r':
+        if key.char == MOVE_HOTKEY:
             mouse.press(Button.right)
             mouse.release(Button.right)
     except AttributeError:
@@ -22,20 +27,16 @@ def on_press(key):
 def on_release(key):
     return True
 #    try:
-#        if key.char == 'a':
-#            print('attack')
+#        if key.char == ATTACK_HOTKEY:
 #            mouse.release(Button.left)
-#        if key.char == 'r':
-#            print('move')
+#        if key.char == MOVE_HOTKEY:
 #            mouse.release(Button.right)
 #    except AttributeError:
 #        return True
-#    if key == Key.esc:
-#        # Stop listener
-#        return False
 
 # Collect events until released
 with Listener(
         on_press=on_press,
-        on_release=on_release) as listener:
+        on_release=on_release
+    ) as listener:
     listener.join()
